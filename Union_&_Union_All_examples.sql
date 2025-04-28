faaf4e6f5c07d7df2ce97e7d23203ad3150b5e8f
Purushottam Gutthe <purushottamgutthe6767@gmail.com>
2025-04-28 21:20:02 +0530

    Union examples started

---
+++ Union_&_Union_All_examples.sql
@@ -0,0 +1,12 @@
+/**
+Union is type of join that allows to fetch  data from multiple select queries
+Union removes the duplicate in case of union 
+but Union All allows the duplicate records. 
+
+There are some conditions
+1. should have same n of columns
+2. should have same or compatible data types
+3.
+*/
+
+
