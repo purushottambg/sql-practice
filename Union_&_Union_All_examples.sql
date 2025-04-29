1cab1225a485568785c78de04594480e7ca9be7c
Purushottam Gutthe <purushottamgutthe6767@gmail.com>
2025-04-29 12:15:19 +0530

    Solved first two challenges

--- Union_&_Union_All_examples.sql
+++ Union_&_Union_All_examples.sql
@@ -7,6 +7,28 @@ There are some conditions
 1. should have same n of columns
 2. should have same or compatible data types
 3.
-*/
+*/ 
+
+-- Example 1: UNION vs UNION ALL
+-- Get a list of all employees from both locations (without duplicates)
+select department from headquarters_employees he
+Union  
+select department from branch_employees be
+;
+
+-- Example 2: Get all employees from the both locations (include duplicates)
+select customer_id, first_name, last_name, email, signup_date from customers_union
+union all
+select employee_id, first_name, last_name, email, hire_date from branch_employees
+union all
+select employee_id, first_name, last_name, email, hire_date from branch_employees;
+
+show tables;
+select * from customers_union;
+select * from branch_employees;
+select * from headquarters_employees;
 
 
+select * from headquarters_employees order by salary;
+select * from headquarters_employees
+order by salary desc limit 3, 2;
